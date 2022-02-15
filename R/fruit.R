5b5e65014679be9612db3aa70c7517e8d6d07e66
annette-hilton <ahilton@ucsb.edu>
2022-02-15 11:30:35 -0800

    Initial commit

---
+++ R/fruit.R
@@ -0,0 +1,13 @@
+#' Wow Fruits Wow
+#'
+#' @param apples
+#' @param bananas
+#' @param oranges
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fruit <- function(apples, bananas, oranges) {
+  (apples + bananas - 2)/ oranges
+}
---
+++ R/lunahunger.R
@@ -0,0 +1,12 @@
+#' LunaHunger
+#'
+#' @param hours
+#' @param food
+#'
+#' @return
+#' @export
+#'
+#' @examples
+lunahunger <- function(hours, food) {
+  (hours*food)* 100
+}
